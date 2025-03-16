26ddce71a2c0efe9b14d2881712f2c5ab40a9d49
Sipan <60195494+RoyalArm001@users.noreply.github.com>
2025-03-16 19:13:32 +0000

    # Ստեղծում ենք commit

---
+++ assets/scripts/firebase-config.js
@@ -0,0 +1,15 @@
+// Firebase Configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDmYFRAIqX2DYxuXeIjXucae3rzTIcSUJo",
+    authDomain: "royalarm-da6c1.firebaseapp.com",
+    databaseURL: "https://royalarm-da6c1.firebaseio.com",
+    projectId: "royalarm-da6c1",
+    storageBucket: "royalarm-da6c1.firebasestorage.app",
+    messagingSenderId: "529674300927",
+    appId: "1:529674300927:web:3c73f1bd8f193c65584bca",
+    measurementId: "G-1W0NQDF9GD"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
