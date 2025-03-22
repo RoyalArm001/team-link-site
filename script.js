1093fb64aa93b32473321bba6d09455c2537d5f2
Sipan <60195494+RoyalArm001@users.noreply.github.com>
2025-03-22 15:14:57 +0000

    Added Firebase Authentication with improved UI

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 // Ռեսպոնսիվ մենյուի համար
 window.addEventListener('resize', function() {
-  const menu = document.querySelector('.menu');
+  const menu = document.querySelector('.top-right-menu');
   if (window.innerWidth < 768) {
     menu.style.flexDirection = 'column';
     menu.style.position = 'fixed';
-    menu.style.bottom = '0';
-    menu.style.left = '0';
-    menu.style.right = '0';
+    menu.style.top = '20px';
+    menu.style.right = '20px';
   } else {
-    menu.style.flexDirection = 'row';
-    menu.style.position = 'relative';
+    menu.style.flexDirection = 'column';
+    menu.style.position = 'fixed';
+    menu.style.top = '20px';
+    menu.style.right = '20px';
   }
 });
