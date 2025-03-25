881c7c628068ffab4d2c335f0d698eb6d37baaa7
Sipan <60195494+RoyalArm001@users.noreply.github.com>
2025-03-25 10:39:50 +0000

    Added Firebase Authentication with improved UI

--- menu/menu.js
+++ menu/menu.js
@@ -2,27 +2,32 @@
 document.addEventListener('DOMContentLoaded', () => {
     const menuHTML = `
       <nav class="tl-menu">
-        <a href="../index.html" class="tl-btn" data-title="">
-          <img src="../menu/svgs/home.svg" class="tl-icon">
-        </a>
-        <a href="games.html" class="tl-btn" data-title="Games">
-          <img src="../menu/svgs/games.svg" class="tl-icon">
-        </a>
-        <a href="dashboard.html" class="tl-btn" data-title="Dashboard">
-          <img src="../menu/svgs/dashboard.svg" class="tl-icon">
-        </a>
-      
-        
-        <a href="login.html" class="tl-btn" data-title="Login">
-          <img src="../menu/svgs/login.svg" class="tl-icon">
-        </a>
-        <a href="contact.html" class="tl-btn" data-title="Contact">
-          <img src="../menu/svgs/contact.svg" class="tl-icon">
-        </a>
-          <a href="about.html" class="tl-btn" data-title="About">
-          <img src="../menu/svgs/about.svg" class="tl-icon">
-        </a>
-      </nav>
+  <!-- Գլխավոր էջ (index.html) -->
+  <a href="/index.html" class="tl-btn" data-title="Home">
+    <img src="/menu/svgs/home.svg" class="tl-icon">
+  </a>
+  
+  <!-- Էջեր /pages պապկից -->
+  <a href="/pages/games.html" class="tl-btn" data-title="Games">
+    <img src="/menu/svgs/games.svg" class="tl-icon">
+  </a>
+  
+  <a href="/pages/dashboard.html" class="tl-btn" data-title="Dashboard">
+    <img src="/menu/svgs/dashboard.svg" class="tl-icon">
+  </a>
+  
+  <a href="/pages/login.html" class="tl-btn" data-title="Login">
+    <img src="/menu/svgs/login.svg" class="tl-icon">
+  </a>
+  
+  <a href="/pages/contact.html" class="tl-btn" data-title="Contact">
+    <img src="/menu/svgs/contact.svg" class="tl-icon">
+  </a>
+  
+  <a href="/pages/about.html" class="tl-btn" data-title="About">
+    <img src="/menu/svgs/about.svg" class="tl-icon">
+  </a>
+</nav>
     `;
     
     document.body.insertAdjacentHTML('beforeend', menuHTML);
