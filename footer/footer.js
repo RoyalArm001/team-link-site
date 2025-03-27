9319265b1307e16f270d61f624c5462e541c558b
Sipan <60195494+RoyalArm001@users.noreply.github.com>
2025-03-27 10:51:07 +0000

    Added Firebase Authentication with improved UI

--- footer/footer.js
+++ footer/footer.js
@@ -1,9 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-    fetch("footer/footer.html")
-      .then((response) => response.text())
-      .then((data) => {
-        document.getElementById("footer-container").innerHTML = data;
-      })
-      .catch((error) => console.error("Footer loading error:", error));
-  });
-  
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Footer script loaded successfully!");
+});
